895183d5cb4bae4657767584a15d07e47cca7e5c
Eduardo <lalithohb@gmail.com>
2024-10-15 14:00:12 -0600

    reesctructura

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import flet as ft
-from taskControl import taskControl
 
+class taskControl(ft.Checkbox):
+  def __init__(self, label_text):
+    super().__init__()
+    self.label = label_text
+    self.on_change = self.taskDone
+
+  def taskDone(self, e):
+    if self.value:
+      self.label_style = ft.TextStyle(
+        decoration=ft.TextDecoration.LINE_THROUGH)
+    else:
+      self.label_style = ft.TextStyle(
+        decoration=ft.TextDecoration.NONE)
+    self.update()
 
 def main(page: ft.Page):
   page.title = "To-Do App"  # Page title
